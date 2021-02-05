29edc3b021be8a3fd9ecd1e563b23595dd1768a7
Airoure <15059198089@163.com>
2021-02-05 17:59:00 +0800

    day 1

---
+++ app/src/main/java/com/zjl/mywanandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zjl.mywanandroid
+
+import android.os.Bundle
+import com.zjl.mylibrary.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun getLayout() = R.layout.activity_main
+    override fun init(savedInstanceState: Bundle?) {
+        super.init(savedInstanceState)
+    }
+}
--- app/src/main/java/com/zjl/mywanandroid/SplashActivity.kt
+++ app/src/main/java/com/zjl/mywanandroid/SplashActivity.kt
@@ -1,13 +1,42 @@
 package com.zjl.mywanandroid
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.permissionx.guolindev.PermissionX
 import com.zjl.mylibrary.base.BaseActivity
+import java.util.*
 
 class SplashActivity : BaseActivity() {
     override fun getLayout() = R.layout.activity_splash
-
+    private var intentTask:Timer? = null
     override fun init(savedInstanceState: Bundle?) {
-        super.init(savedInstanceState)
+       PermissionX.init(this)
+           .permissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+           .request { allGranted, grantedList, deniedList ->
+               if(allGranted){
+                   intentTask = Timer()
+                   intentTask!!.schedule(object :TimerTask(){
+                       override fun run() {
+                           startIntent()
+                       }
+                   },2500)
+               }else{
+
+               }
+           }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        intentTask?.cancel()
+    }
+
+    private fun startIntent() {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
